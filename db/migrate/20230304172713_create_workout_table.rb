87b90753103808d3ac67de330756cb523a3d56b8
aronianm <aronian.m@gmail.com>
2023-03-04 12:32:22 -0500

    added migration for workouts and models

---
+++ db/migrate/20230304172713_create_workout_table.rb
@@ -0,0 +1,20 @@
+class CreateWorkoutTable < ActiveRecord::Migration[7.0]
+  def change
+    create_table :exercise_workouts do |t|
+      t.integer :workout_id
+      t.integer :exercise_id
+      t.timestamps
+    end
+    create_table :workouts do |t|
+      t.string :name
+      t.string :day
+      t.text :notes
+      t.integer :created_by
+      t.integer :updated_by
+      t.timestamps
+    end
+
+    add_foreign_key :exercise_workouts, :workouts, column: :workout_id
+    add_foreign_key :exercise_workouts, :exercises, column: :exercise_id
+  end
+end
